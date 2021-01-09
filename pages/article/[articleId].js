fdbebcb31ae5356903f0f367faba337b902e3ff5
Daniel Clarke <daniel@danielrlc.com>
2021-01-09 16:15:57 +0200

    routing to SSR articles

--- pages/article/[articleId].js
+++ pages/article/[articleId].js
@@ -1,22 +1,64 @@
 import Nav from '../../components/nav'
-import {useRouter} from 'next/router'
 
-// // This gets called on every request
-// export async function getServerSideProps() {
-//   const router = useRouter()
-//   const {articleId} = router.query
-//   // Fetch data from external API
-//   const res = await fetch(`https://lettera.api.ksfmedia.fi/v3/frontpage`)
-//   const data = await res.json()
-//   // Pass data to the page via props
-//   return { props: { data } }
-// }
+// This gets called on every request
+export async function getServerSideProps({ params }) {
+  const res = await fetch(
+    `https://lettera.api.ksfmedia.fi/v3/article/${params.articleId}`,
+  )
+  const data = await res.json()
+  // Pass data to the page via props
+  return { props: { data } }
+}
 
-function Article() {
-  const router = useRouter()
-  const {articleId} = router.query
+function Article({ data }) {
+  console.log(data)
+  const {
+    authors,
+    body,
+    listImage,
+    mainImage,
+    preamble,
+    publishingTime,
+    shareUrl,
+    title,
+  } = data
   return (
-    <p>Article: {articleId} </p>
+    <div>
+      <Nav />
+      <div className="px-10 py-4">
+        <h1 className="text-4xl mb-6">{title}</h1>
+        <figure className="mb-4">
+          <img src={listImage.url} alt={listImage.caption} className="w-full" />
+          <figcaption className="text-gray-500">
+            {listImage.caption} {listImage.byline}
+          </figcaption>
+        </figure>
+        {body.map(({ html, headline, image, box }, i) => {
+          return html ? (
+            <p
+              key={i}
+              className="text-gray-700 mb-4"
+              dangerouslySetInnerHTML={{ __html: html }}
+            />
+          ) : headline ? (
+            <h2 key={i} className="text-2xl font-bold text-gray-700 mb-2 mt-6">
+              {headline}
+            </h2>
+          ) : image ? (
+            <figure key={i} className="mb-4">
+              <img src={image.url} alt={image.caption} className="w-full" />
+              <figcaption className="text-gray-500">
+                {image.caption} {image.byline}
+              </figcaption>
+            </figure>
+          ) : box ? (
+            box.content.map((text, i) => {
+              return <p key={i} dangerouslySetInnerHTML={{ __html: text }} />
+            })
+          ) : null
+        })}
+      </div>
+    </div>
   )
 }
 
--- pages/index.js
+++ pages/index.js
@@ -5,9 +5,11 @@ import Nav from '../components/nav'
 export async function getServerSideProps() {
   // Fetch data from external API
   const res = await fetch(`https://lettera.api.ksfmedia.fi/v3/frontpage`)
-  const data = await res.json()
+  let data = await res.json()
   // Pass data to the page via props
-  return { props: { data } }
+  return {
+    props: { data },
+  }
 }
 
 function IndexPage({ data }) {
@@ -17,7 +19,7 @@ function IndexPage({ data }) {
       <Nav />
       <h1 className="text-3xl font-bold mb-6">Latest articles</h1>
       <ul>
-        {data.map(({title, uuid}) => (
+        {data.map(({ title, uuid }) => (
           <li key={uuid} className="mb-3">
             <Link href={`./article/${uuid}`}>
               <a className="underline text-blue-700">{title}</a>
