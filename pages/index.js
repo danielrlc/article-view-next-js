13083c4d137a45fb8b906b31bf74040bcd3c45b1
Daniel Clarke <daniel@danielrlc.com>
2021-01-09 13:39:00 +0200

    routing from home page to latest articles / wip - rendering article page

--- pages/index.js
+++ pages/index.js
@@ -1,45 +1,32 @@
+import Link from 'next/link'
 import Nav from '../components/nav'
-
-function IndexPage({ data }) {
-  console.log(data.body)
-  return (
-    <div>
-      <Nav />
-      <div className="px-10 py-20">
-        <h1 className="text-4xl text-gray-700">
-          {data.title}
-        </h1>
-        {data.body.map(({ html, headline, image, box }, i) => {
-          return html ? (
-            <p
-              key={i}
-              className="text-gray-700"
-              dangerouslySetInnerHTML={{ __html: html }}
-            />
-          ) : headline ? (
-            <h2
-              key={i}
-              className="text-2xl font-bold text-gray-700"
-            >
-              {headline}
-            </h2>
-          ) : null
-        })}
-      </div>
-    </div>
-  )
-}
 
 // This gets called on every request
 export async function getServerSideProps() {
   // Fetch data from external API
-  const res = await fetch(
-    `https://lettera.api.ksfmedia.fi/v3/article/a6282b95-e620-4040-87d1-731fed85a7d6`,
-  )
+  const res = await fetch(`https://lettera.api.ksfmedia.fi/v3/frontpage`)
   const data = await res.json()
-
   // Pass data to the page via props
   return { props: { data } }
+}
+
+function IndexPage({ data }) {
+  console.log(data)
+  return (
+    <div className="px-8">
+      <Nav />
+      <h1 className="text-3xl font-bold mb-6">Latest articles</h1>
+      <ul>
+        {data.map(({title, uuid}) => (
+          <li key={uuid} className="mb-3">
+            <Link href={`./article/${uuid}`}>
+              <a className="underline text-blue-700">{title}</a>
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </div>
+  )
 }
 
 export default IndexPage
